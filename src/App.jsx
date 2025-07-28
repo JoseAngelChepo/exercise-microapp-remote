0cc2a20094cdaf2987b19a259357feebebb3f924
Jose Angel Lopez Mondragon <angel@customleads.ai>
2025-07-28 10:40:56 -0600

    [INIT]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola desde Remote Products 🚀</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.jsx',
+  mode: 'development',
+  devServer: {
+    port: 3001,
+    static: './dist',
+  },
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app_products',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ProductsApp': './src/App',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^19.1.1' },
+        'react-dom': { singleton: true, requiredVersion: '^19.1.1' },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
