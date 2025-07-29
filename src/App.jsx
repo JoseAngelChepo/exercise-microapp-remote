e51d37b635841c32a4e438bf570e721f62bc949c
Jose Angel Lopez Mondragon <angel@customleads.ai>
2025-07-29 11:14:52 -0600

    [ADD] tailwind 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Products remote</h1>
+      <h1 className="bg-blue-500">Products remote</h1>
     </>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
       }
     ],
   },
